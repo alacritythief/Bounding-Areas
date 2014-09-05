9e83d9c5ad4fd15c0f8feb43cb3ccee3c34aaf08
Andy Wong <alacritythief@gmail.com>
2014-09-05 15:47:23 -0400

    Rspec cleared. Complete.

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,21 @@
 class BoundingArea
+  attr_reader :boxes
+
+  def initialize(array)
+    @boxes = array
+  end
+
+  def contains_point?(x,y)
+    if @boxes == []
+      false
+    else
+      @boxes.each do |box|
+        if box.contains_point?(x,y) == true
+          return true
+        else
+          return false
+        end
+      end
+    end
+  end
 end
